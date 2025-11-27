05f1c2547a20be223d2b0818f497303d4a87af64
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2025-11-27 17:56:00 +0000

    apply formater

--- combnet/load.py
+++ combnet/load.py
@@ -14,28 +14,31 @@ import torch
 
 def partition(dataset):
     """Load partitions for dataset"""
-    with open(combnet.PARTITION_DIR / f'{dataset}.json') as file:
+    with open(combnet.PARTITION_DIR / f"{dataset}.json") as file:
         return json.load(file)
+
 
 def audio(file):
     """Load audio from disk"""
     path = Path(file)
-    if path.suffix.lower() == '.mp3':
+    if path.suffix.lower() == ".mp3":
         try:
-            audio, sample_rate = torchaudio.load(path, format='mp3')
+            audio, sample_rate = torchaudio.load(path, format="mp3")
         except RuntimeError:
             raise RuntimeError(
-                'Failed to load mp3 file, make sure ffmpeg<=4.3 is installed')
+                "Failed to load mp3 file, make sure ffmpeg<=4.3 is installed"
+            )
     else:
         audio, sample_rate = torchaudio.load(file)
 
     # Maybe resample
     return combnet.resample(audio, sample_rate)
 
+
 def model(checkpoint=combnet.DEFAULT_CHECKPOINT) -> torch.nn.Module:
-    state_dict = torch.load(checkpoint, map_location='cpu', weights_only=True)
-    if 'model' in state_dict:
-        state_dict = state_dict['model']
+    state_dict = torch.load(checkpoint, map_location="cpu", weights_only=True)
+    if "model" in state_dict:
+        state_dict = state_dict["model"]
 
     model = combnet.Model()
 
